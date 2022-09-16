40afdfbe63a23bb66dce2b08a6e888ed8dbb7cff
hosanyoon <yoonhosan@naver.com>
2022-09-16 09:30:36 +0900

    dto

--- back_pj/src/dto/Video.java
+++ back_pj/src/dto/Video.java
@@ -1,5 +1,9 @@
 package dto;
 
 public class Video {
-
+	private String title;
+	private String fitPartName;
+	private String youtubeId;
+	private String channelName;
+	private int viewCnt;
 }
--- back_pj/src/servlet/MainController.java
+++ back_pj/src/servlet/MainController.java
@@ -1,5 +1,18 @@
 package servlet;
 
-public class MainController {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/main")
+public class MainController extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
+	}
 }
